209231932a5d01674c8ea3e6cf653e75493fac4d
Lexa Huang <sixian.duidui@gmail.com>
2024-05-19 20:36:20 -0400

    added code to append names and image urls to .js file

--- scrape.py
+++ scrape.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 
 root_url = "https://kpop.fandom.com/"
 
 
 #these are the pages i think are the most relevant 
-group_category_urls = [
+category_urls = [
     "/wiki/Category:Female_groups",
     "/wiki/Category:Male_groups",
     "/wiki/Category:Male_soloists",
@@ -14,7 +15,7 @@ group_category_urls = [
 ]
 
 #some groups i want to manually add 
-group_urls = [
+additional_group_urls = [
     "/wiki/NCT",
     "/wiki/MONSTA_X",
     "/wiki/Stray_Kids",
@@ -36,10 +37,10 @@ def get_html(url):
     return soup
 
 #get trending groups
-def get_trending_groups(category_page_url):
+def get_trending_groups(category_url):
     '''given the page url of a group category, return a list of page urls of trending groups'''
 
-    soup = get_html(category_page_url)
+    soup = get_html(category_url)
     trending_div = soup.find('ul', class_='category-page__trending-pages')
     hrefs = [a['href'] for a in trending_div.find_all('a')]
 
@@ -47,10 +48,10 @@ def get_trending_groups(category_page_url):
 
 
 #get members of trending groups
-def get_members(group_page_url):
+def get_members(group_url):
     '''given the page url of a group, return a list of page urls of all members'''
     
-    soup = get_html(group_page_url)
+    soup = get_html(group_url)
     current_div = soup.find('div', attrs={'data-source': 'current'})
     hrefs = [a['href'] for a in current_div.find_all('a')]
 
@@ -70,16 +71,51 @@ def get_idol_data(member_url):
     image_figure = soup.find('figure', attrs={'data-source': 'image'})
     image_url = image_figure.find('img')['src']
 
-    return(name, image_url)
+    return {'name': name, 'image_url': image_url} 
 
 
 if __name__ == "__main__":
     # test_category_page = "/wiki/Category:Female_groups"
     # print(get_trending_groups(test_category_page))
 
-    test_group_page = "wiki/NCT"
-    print(get_members(test_group_page))
+    # test_group_page = "wiki/NCT"
+    # print(get_members(test_group_page))
 
     # test_member_page = "/wiki/Pharita"
     # print(get_idol_data(test_member_page))
+
+    #full list of groups 
+    group_urls = []
+    for category_url in category_urls:
+        group_urls += get_trending_groups(category_url)
+
+    group_urls += additional_group_urls
+    
+
+    #write to json file
+    with open('idol_data.js', 'w') as f:
+        f.write('const idolData = [\n')
+
+        for group_url in group_urls:
+
+            #captures all irregular html 
+            try: 
+                member_urls = get_members(group_url)
+                for member_url in member_urls:
+                    
+                    idol_data = get_idol_data(member_url)
+                    json.dump(idol_data, f, indent=2)
+                    f.write(',')
+            except:
+                continue 
+                
+
+        
+        f.write(']')
+    
+    f.close()
+
+
+                
+
 
